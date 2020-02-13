bb11cf24c0e86d91fc6cb096f1c683115350c428
lbilde <ljb@easv.dk>
2020-02-13 12:23:56 +0100

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDNF-LYJkeqZRjQC7yy_SxoSC8jYCkJ5sA',
+    authDomain: 'hairdresser-salon-1d12f.firebaseapp.com',
+    databaseURL: 'https://hairdresser-salon-1d12f.firebaseio.com',
+    projectId: 'hairdresser-salon-1d12f',
+    storageBucket: 'hairdresser-salon-1d12f.appspot.com',
+    messagingSenderId: '627549879342',
+    appId: '1:627549879342:web:bd56eee43bd2660ce4b3bf',
+    measurementId: 'G-RGN28JY7SN'
+  }
 };
 
 /*

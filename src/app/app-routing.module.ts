af423de9da4bd1cff088c5af012de9e9ef90b5a9
lbilde <ljb@easv.dk>
2020-02-18 10:44:45 +0100

    Adding routing and welcomepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 const routes: Routes =
-  [{ path: 'products',
-    loadChildren: () => import('./products/products.module')
-      .then(m => m.ProductsModule) }];
+  [
+    { path: 'products',
+      loadChildren: () =>
+        import('./products/products.module')
+          .then(m => m.ProductsModule) },
+    {path: 'welcome',
+      loadChildren: () =>
+        import('./welcome/welcome.module')
+          .then(m => m.WelcomeModule) },
+    { path: '',
+      redirectTo: '/welcome',
+      pathMatch: 'full' },
+    { path: '**',
+      redirectTo: '/welcome',
+      pathMatch: 'full'  }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

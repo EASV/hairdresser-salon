c9dc33464e028d60e46ca01a0d172a07399a04c6
lbilde <ljb@easv.dk>
2020-04-22 13:08:43 +0200

    Added alot of stuff

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -2,12 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ProductsComponent } from './overview/products.component';
-import {CreateProductComponent} from './create/create-product.component';
 
 const routes: Routes = [
-  { path: 'create',
-    component: CreateProductComponent
-  },
   { path: '',
     component: ProductsComponent
   }];
